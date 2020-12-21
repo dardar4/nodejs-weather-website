20edd0ae514dd52b2baa2f679df10c5e49278428
Amir <dardar4@gmail.com>
2020-12-21 09:25:22 +0200

    Update app.js
    fixed a typo

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ app.get('/help', (req, res) => {
 app.get('/weather', (req, res) => {
     if(!req.query.address){
         return res.send({
-            error: 'Address is mandetory'
+            error: 'Address is mandatory'
         })
     }
 
